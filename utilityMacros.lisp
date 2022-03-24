de8dc31f00a3d2e820731a778a1c795db95cb473
Isaac M <30935172+IdmFoundInHim@users.noreply.github.com>
2022-03-24 13:30:17 -0400

    Finish calculating board points, add geography schematic

--- utilityMacros.lisp
+++ utilityMacros.lisp
@@ -2,9 +2,9 @@
   `(let ,(loop for sym in symbols collect `(,sym (gensym)))
      ,@body))
 
-(defmacro +elementwise (seq1 seq2)
+(defmacro +vector (vec1 vec2)
   (with-gensyms (a b)
-    `(loop for ,a in ,seq1 for ,b in ,seq2 collecting (+ ,a ,b))))
+    `(loop for ,a across ,vec1 for ,b across ,vec2 collecting (+ ,a ,b))))
 
 (defmacro make-adjustable-vector (&rest extra-args)
   `(make-array 1 :fill-pointer 0 :adjustable t ,@extra-args))
